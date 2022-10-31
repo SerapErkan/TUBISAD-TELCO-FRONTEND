cee54d2c55a4a761d2e32c9c45f6621718204008
SERAP KANTAROĞLU <serapkantaroglu.59@hotmail.com>
2022-10-31 16:58:29 +0300

    Initial commit

---
+++ telco-frontend/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CategoriesComponent } from './components/categories/categories.component';
+import{ HttpClientModule}from '@angular/common/http'
+import { CategoriesService } from './services/categories.service';
+import { FormsModule } from '@angular/forms';
+@NgModule({
+  declarations: [
+    AppComponent,
+    CategoriesComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,HttpClientModule,FormsModule
+   
+  ],
+  providers: [CategoriesService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
