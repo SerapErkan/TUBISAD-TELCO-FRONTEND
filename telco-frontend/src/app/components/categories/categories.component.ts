ebb6cb33d1e696117831efc78c7120f507fb6abe
SERAP KANTAROĞLU <serapkantaroglu.59@hotmail.com>
2022-11-01 08:41:15 +0300

    category update ()

--- telco-frontend/src/app/components/categories/categories.component.ts
+++ telco-frontend/src/app/components/categories/categories.component.ts
@@ -20,39 +20,48 @@ export class CategoriesComponent implements OnInit {
 
   categories!: Category[];
   categoryAddForm!: FormGroup;
-  categoryIdToDelete: number = 0; 
+  categoryIdToDelete: number = 0;
 
-  isEdited:boolean= false;
+  isEdited: boolean = false;
+  isAdded: boolean = true;
+  isDeleted: boolean = false;
+
+  categoryDescription: string = "";
+  categoryName: string = "";
+  categoryId: number = 0;
+
+
   error: string = '';
-
-  categoryDescription:string="";
-  categoryName:string="";
-  categoryId:number=0;
   constructor(
     private categoriesService: CategoriesService,
     private formBuilder: FormBuilder
-  ) {}
+  ) { }
 
   ngOnInit(): void {
     this.getByCategories();
     this.AddForm();
+    console.log("a:", this.isAdded,   "d:", this.isDeleted,"e:",this.isEdited);
   }
 
-AddForm(){
+  AddForm() {
 
     this.categoryAddForm = this.formBuilder.group({
+      id: new FormControl(null,
+        [
+          Validators.required,
+        ]),
       name: new FormControl(null,
         [
           Validators.required,
           Validators.minLength(5),
         ]),
-        description: new FormControl(null,
-          [
-            Validators.required,
-            Validators.minLength(5),
-            Validators.maxLength(20),
-          ]),
-   
+      description: new FormControl(null,
+        [
+          Validators.required,
+          Validators.minLength(5),
+          Validators.maxLength(20),
+        ]),
+
     });
   }
 
@@ -62,87 +71,125 @@ AddForm(){
     });
   }
 
-  addCategoryById(): void {
+  add_edit_CategoryById(): void {
 
-    const category: Category = {
-      ...this.categoryAddForm.value,
-    };
-    this.categoriesService.addCategory(category).subscribe({
-      next: (response) => {
-        console.info(`Category(${response.id}) has added.`);
-      },
-      error: (err) => {
-        console.log(err);
+    if (this.isEdited === true) {
+      const category: Category = {
+        ...this.categoryAddForm.value,
+      };
+      this.categoriesService.updateCategory(category).subscribe({
+        next: (res) => {
+          console.info(`Category ${category.id} has updated`);
+        
+        },
+        error: (err) => {
+          console.log(category);
+          console.error(err);
+        },
+        complete: () => {
+          this.isEdited = false;
+          this.categoryAddForm.reset();
+          this.getByCategories();
+        },
+      });
 
-        this.error = err.statusText;
-      },
-      complete: () => {
-        if (this.error) this.error = '';
-        this.categoryAddForm.reset();
-        this.getByCategories();
-      },
+      this.isEdited = false;
+    }
+    if(this.isDeleted === true){
+
+      this.categoriesService.deleteCategory(this.categoryId).subscribe( {
+        next: (res) => {
+          console.info(`Category(${this.categoryId}) has deleted.`);
+        },
+        error: (err) => {
+          console.log(err);
+
+          this.error = err.statusText;
+        },
+        complete: () => {
+          if (this.error) this.error = '';
+          this.categoryAddForm.reset();
+          this.isDeleted=false;
+          this.getByCategories();
+        },
     });
+    
+    this.isDeleted =false;
+
+    }
+    if(this.isAdded===true){
+          const category: Category = {
+        ...this.categoryAddForm.value}
+        
+      this.categoriesService.addCategory(category).subscribe({
+        next: (response) => {
+          console.info(`Category(${response.id}) has added.`);
+        },
+        error: (err) => {
+          console.log(err);
+
+          this.error = err.statusText;
+        },
+        complete: () => {
+          if (this.error) this.error = '';
+          this.categoryAddForm.reset();
+          this.getByCategories();
+        },
+      });
+
+      }
+
+
+
+    }
+  
+    
+  
+
+
+ 
+
+  // deleteCategoriesById(id:number) {
+  //   if(id !== null){
+  //     this.isDeleted=true;
+  //   this.categoriesService.deleteCategory(id).subscribe(() => {
+  //     this.getByCategories();
+  //   });
+  // }
+  //   }
+
+  deleteCategoriesById(item:any) {
+    this.categoryDescription = item.description;
+    this.categoryName = item.name;
+    this.categoryId = item.id; 
+    this.isDeleted=true;
+  }
+
+  updateCategory(item: any) {
+    this.categoryId = item.id;
+    this.categoryDescription = item.description;
+    this.categoryName = item.name;
+    this.isEdited = true;
+    this.isAdded=false;
+    this.isDeleted=false
+
+
   }
 
 
+  // showErrors() {
+  //   const { dirty, touched, errors } = this.categoryAddForm
+  //   return dirty && touched && errors;
+  // }
 
   deleteCategories() {
-    this.categoriesService.deleteCategory(this.categoryIdToDelete).subscribe(() => {
-      this.categoryIdToDelete=0;
+    this.categoriesService.deleteCategory(this.categoryId).subscribe(() => {
+      this.categoryId = 0;
       this.getByCategories();
     });
-    }
+  }
 
-    deleteCategoriesById(id:number) {
-      if(id !== null){
-      this.categoriesService.deleteCategory(id).subscribe(() => {
-        this.getByCategories();
-      });
-    }
-      }
-    
-   
-updateCategory(item:any){
-// console.log(item) //control
-this.categoryDescription=item.description;
-this.categoryName=item.name
-this.categoryId=item.id
-this.isEdited=true;
+
 }
-
-
-
-
-   updateCategoriesById(): void {
-    const category: Category = {
-      id:this.categoryId,
-      ...this.categoryAddForm.value,
-    };
-    this.categoriesService.updateCategory(category).subscribe({
-      next: (res) => {
-        console.info(`Category ${category.id} has updated`);
-        this.isEdited=false;
-      },
-      error: (err) => {
-        console.log(category);
-        console.error(err);
-      },
-      complete: () => {
-        this.isEdited = false;
-        this.categoryAddForm.reset();
-        this.getByCategories();
-      },
-    });
-  }
-    
-
-
-    // showErrors() {
-    //   const { dirty, touched, errors } = this.categoryAddForm
-    //   return dirty && touched && errors;
-    // }
-
-   
-  }
 
 
